8cb65ff466992be333340dc0893393935591ec8d
OneStepCloser <artinspira@gmail.com>
2017-11-20 23:30:45 +0300

    I'm halfway done

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -7,29 +7,49 @@ import {
 
 import './Sidebar.css';
 
-export default () => (
-  <div className='Sidebar'>
-    <div className='Sidebar__header'>
-      Счета
-    </div>
-
-    <NavLink to='/account/321321' className='Link' activeClassName='Link--active'>
-      <div className='Sidebar__account'>
-        <div className='Sidebar__account-name'>
-          Основной
-        </div>
-        <div className='Sidebar__account-amount'>
-          <Money value={2133.231} />
-        </div>
+export default class Sidebar extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return <div className='Sidebar'>
+      <div className='Sidebar__header'>
+        Счета
       </div>
-    </NavLink>
 
-    <NavLink to='/create-account' className='Link' activeClassName='Link--active'>
-      <div className='Sidebar__account'>
-        <div className='Sidebar__account-name'>
-          Добавить счет
+      {
+        Object.keys(this.props.accounts).map(accountId => (
+          <NavLink key={accountId} to={'/account/' + accountId} className='Link' activeClassName='Link--active'>
+            <div className='Sidebar__account'>
+              <div className='Sidebar__account-name'>
+                {this.props.accounts[accountId].name}
+              </div>
+              <div className='Sidebar__account-amount'>
+                <Money value={this.props.accounts[accountId].amount} currency={this.props.accounts[accountId].currency}/>
+              </div>
+            </div>
+          </NavLink>
+        ))
+      }
+
+      {/*<NavLink to='/account/321321' className='Link' activeClassName='Link--active'>*/}
+      {/*<div className='Sidebar__account'>*/}
+      {/*<div className='Sidebar__account-name'>*/}
+      {/*Основной*/}
+      {/*</div>*/}
+      {/*<div className='Sidebar__account-amount'>*/}
+      {/*<Money value={2133.231} />*/}
+      {/*</div>*/}
+      {/*</div>*/}
+      {/*</NavLink>*/}
+
+      <NavLink to='/create-account' className='Link' activeClassName='Link--active'>
+        <div className='Sidebar__account'>
+          <div className='Sidebar__account-name'>
+            Добавить счет
+          </div>
         </div>
-      </div>
-    </NavLink>
-  </div>
-);
+      </NavLink>
+    </div>;
+  }
+}
--- src/pages/CreateAccount.js
+++ src/pages/CreateAccount.js
@@ -1,8 +1,89 @@
 import React from 'react';
 
-export default () => (
-  <div>
-    <h2>Создание аккаунта</h2>
-    <p>Здесь мы будем создавать новые счета</p>
-  </div>
-);
+import FormField from '../components/FormField/FormField';
+import FormText from '../components/FormText/FormText';
+import Form from '../components/Form/Form';
+import Button from '../components/Button/Button';
+
+export default class CreateAccount extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      name: '',
+      currency: '',
+      description: ''
+    };
+  }
+
+  rules = {
+    name: [
+      val => val.length !== 0
+    ],
+    currency: [
+      val => val.length !== 0,
+      val =>  ['RUB', 'USD', 'GBP', 'EUR'].indexOf(val.toUpperCase()) >= 0
+    ],
+    description: [
+      val => val.length !== 0
+    ]
+  };
+
+  formClear() {
+    this.setState({
+      name: '',
+      currency: '',
+      description: ''
+    });
+  }
+
+
+  validationsForm() {
+    return Object.keys(this.rules).every(key => this.rules[key].every(rule => rule(this.state[key])));
+  }
+
+  handleInputChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+
+    if (!this.validationsForm()) {
+      return;
+    }
+
+    this.props.createAcoount(this.state);
+
+    this.formClear();
+  };
+
+  render() {
+    return (
+      <div>
+        <Form onSubmit={this.handleSubmit}>
+          <FormField label="Имя" name="name">
+            <FormText value={this.state.name} name="name" onChange={this.handleInputChange}/>
+          </FormField>
+          <FormField label="Валюта" name="currency">
+            <FormText value={this.state.currency} name="currency" onChange={this.handleInputChange}/>
+          </FormField>
+          <FormField label="Описание" name="description">
+            <FormText value={this.state.description} name="description" onChange={this.handleInputChange}/>
+          </FormField>
+          <Button type="submit">Отправить</Button>
+        </Form>
+      </div>
+    );
+  }
+}
+
+// export default () => (
+//   <div>
+//     <h2>Создание аккаунта</h2>
+//     <p>Здесь мы будем создавать новые счета</p>
+//   </div>
+// );
